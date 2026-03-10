1ff3f99585b2fe4e6cfff370b26761d6b08cd00f
daulyoi <derp.daffnet@gmail.com>
2026-03-10 08:08:19 +0700

    feat(auth): auth dependencies

---
+++ backend/app/api/dependencies.py
@@ -0,0 +1,80 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import get_db
+from app.core.security import Security
+from app.services.user_service import UserService
+from app.services.auth_service import AuthService
+from app.schemas.user import UserResponse
+from app.repositories import user_repository
+from app.enums.user_enums import UserRoles
+
+# OAuth2 scheme for token-based authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_user_service(db: AsyncSession = Depends(get_db)) -> UserService:
+    repo = user_repository.UserRepository(db)
+    return UserService(repo)
+
+async def get_auth_service(db: AsyncSession = Depends(get_db)) -> AuthService:
+    repo = user_repository.UserRepository(db)
+    return AuthService(repo)
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        security: Security = Depends(Security),
+        user_service: UserService = Depends(get_user_service),
+) -> UserResponse:
+    """
+    Dependency function to retrieve the current authenticated user based on the provided token.
+    
+    :param token: The OAuth2 token extracted from the request header
+    :type token: str
+    :param db: The database session dependency
+    :type db: AsyncSession
+    :return: The UserResponse object representing the current authenticated user
+    :rtype: UserResponse
+    :raises HTTPException: If the token is invalid or the user is not found
+    """
+    # print(f"DEBUG: Token received by FastAPI: '{token}'")
+
+    # Decode and validate the token 
+    print(f"DEBUG: Original token: {token}")
+    payload = security.decode_token(token)
+    # print(f"DEBUG: Payload extracted from token: {payload}")
+
+    # Get the user ID from the token payload
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token: missing user ID")
+    
+    # Retrieve the user from the database
+    current_user = await user_service.get_current_user(int(user_id))
+    if not current_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    
+    return current_user
+
+def ensure_is_admin(current_user: UserResponse = Depends(get_current_user)) -> bool:
+    """
+    Dependency function to ensure the current authenticated user has admin privileges.
+    
+    :param current_user: The current active user retrieved from the get_current_user dependency
+    :type current_user: UserResponse
+    :raises HTTPException: If the user does not have admin privileges
+    """
+    if current_user.role != UserRoles.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+    return True
+    
+def ensure_is_facility_manager(current_user: UserResponse = Depends(get_current_user)):
+    """
+    Dependency function to ensure the current authenticated user has facility manager privileges.
+    
+    :param current_user: The current active user retrieved from the get_current_user dependency
+    :type current_user: UserResponse
+    :raises HTTPException: If the user does not have facility manager privileges
+    """
+    if current_user.role != UserRoles.FACILITY_MANAGER:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
